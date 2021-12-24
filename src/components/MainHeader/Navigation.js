e8a68463008d9e23614f5fc45b9018322ef3a11c
Felipe Teixeira Schefer <felipe_official@outlook.com>
2021-12-24 15:47:16 -0300

    useRef diving into Forward Refs

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,32 +1,32 @@
-import React, {useContext} from "react";
-import AuthContext from "../../store/auth-context";
+import React, { useContext } from 'react';
 
-import classes from "./Navigation.module.css";
+import AuthContext from '../../store/auth-context';
+import classes from './Navigation.module.css';
 
-const Navigation = (props) => {
-  const ctx = useContext(AuthContext)
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <button onClick={props.onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
- 
+const Navigation = () => {
+  const ctx = useContext(AuthContext);
+
+  return (
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={ctx.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navigation;
